b519914f0b3ca44ab6b7faed27f86215827ff3e1
Mustafa Bulut <androidbulut@gmail.com>
2022-06-23 02:14:20 +0300

    change theme added to settings

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,17 +1,35 @@
-import { Text } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { styles } from './styles';
 import Header from '../../components/Header';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useTheme } from '../../contexts/ThemeContext';
+import { colors } from '../../constants/colors';
+import { ThemeTypes } from '../../contexts/ThemeContext/types';
 
 const Settings = () => {
-  const { themeColor } = useTheme();
+  const { themeColor, changeTheme } = useTheme();
+  const themes = ['light', 'dark', 'blue', 'green', 'gold'];
+
+  const renderThemeCircle = (theme: string) => {
+    return (
+      <TouchableOpacity
+        key={theme}
+        style={[styles.themeOuter, { backgroundColor: colors[theme as ThemeTypes].button }]}
+        onPress={() => changeTheme(theme as ThemeTypes)}
+      >
+        <View style={[styles.themeInner, { backgroundColor: colors[theme as ThemeTypes].background }]} />
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: themeColor.background }]}>
-      <Header title={'settings'} />
-      <Text>Settings</Text>
+      <Header title={'settings'} canGoBack={true} />
+      <View style={[styles.changeContainer, { backgroundColor: themeColor.subStepSection }]}>
+        <Text style={[styles.changeText, { color: themeColor.title }]}>Theme</Text>
+        <View style={styles.themesContainer}>{themes.map((theme) => renderThemeCircle(theme))}</View>
+      </View>
     </SafeAreaView>
   );
 };
--- src/screens/Settings/styles.ts
+++ src/screens/Settings/styles.ts
@@ -4,4 +4,34 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  changeContainer: {
+    height: 112,
+    paddingHorizontal: 24,
+    marginTop: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  themesContainer: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  changeText: {
+    flex: 1,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  themeOuter: {
+    height: 30,
+    width: 30,
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  themeInner: {
+    height: 20,
+    width: 20,
+    borderRadius: 20,
+  },
 });
