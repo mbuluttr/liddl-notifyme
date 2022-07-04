7ac8a9994147b3d780654f343c38b636e0237da3
Mustafa Bulut <androidbulut@gmail.com>
2022-07-05 00:45:43 +0300

    removed unnecessary functions

--- App.tsx
+++ App.tsx
@@ -4,58 +4,14 @@ import ThemeProvider from './src/contexts/ThemeContext';
 import AppStackNavigator from './src/routers/AppStackNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import NotificationHelper from './src/helpers/NotificationHelper';
-import notifee, { EventType } from '@notifee/react-native';
-
-notifee.onBackgroundEvent(async ({ type, detail }) => {
-  const { notification, pressAction } = detail;
-  console.log(
-    `[onBackgroundEvent] notification id: ${notification?.id},  event type: ${EventType[type]}, press action: ${pressAction?.id}`,
-  );
-  // Check if the user pressed the "Mark as read" action
-  if (type === EventType.ACTION_PRESS && pressAction?.id === 'first_action_reply') {
-    console.log('[onBackgroundEvent] ACTION_PRESS: first_action_reply');
-
-    // Remove the notification
-    if (notification?.id) {
-      await notifee.cancelNotification(notification?.id);
-    }
-  }
-});
 
 const App = () => {
   useEffect(() => {
-    notifee.getTriggerNotificationIds().then((ids) => console.log(ids));
-    NotificationHelper.requestUserPermission();
-  }, []);
+    const getUserPermission = async () => {
+      await NotificationHelper.requestUserPermission();
+    };
 
-  // Subscribe to events
-  useEffect(() => {
-    return notifee.onForegroundEvent(async ({ type, detail }) => {
-      const { notification, pressAction } = detail;
-      const pressActionLabel = pressAction ? `, press action: ${pressAction?.id}` : '';
-      console.log(
-        `[onForegroundEvent] notification id: ${notification?.id},  event type: ${EventType[type]}${pressActionLabel}`,
-      );
-
-      switch (type) {
-        case EventType.DISMISSED:
-          console.log('[onForegroundEvent] User dismissed notification', notification);
-          break;
-        case EventType.PRESS:
-          console.log('[onForegroundEvent] User pressed notification', notification);
-          break;
-        case EventType.ACTION_PRESS:
-          console.log('[onForegroundEvent] User pressed an action', notification, detail.pressAction);
-
-          if (detail.pressAction?.id === 'first_action_reply') {
-            // perform any server calls here and cancel notification
-            if (notification?.id) {
-              await notifee.cancelDisplayedNotification(notification.id);
-            }
-          }
-          break;
-      }
-    });
+    getUserPermission();
   }, []);
 
   return (
